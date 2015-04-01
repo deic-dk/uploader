7538504763fa43777e6e8d737efc340ae2bb94a0
deltafunction <frederik@orellana.dk>
2015-04-01 14:03:35 +0200

    load js

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 \OCP\User::checkLoggedIn();
 \OCP\App::checkAppEnabled('uploader');
 
+OCP\Util::addStyle('chooser', 'jqueryFileTree');
+OCP\Util::addscript('chooser', 'jquery.easing.1.3');
+OCP\Util::addscript('chooser', 'jqueryFileTree');
+OCP\Util::addScript('uploader', 'browse');
+
 \OCP\Util::addScript('uploader', 'html5fileupload');
 \OCP\Util::addScript('uploader', 'html5fileupload_create');
 \OCP\Util::addStyle('uploader', 'html5fileupload');
+
+
 
 $tpl = new OCP\Template("uploader", "main", "user");
 $tpl->printPage();
